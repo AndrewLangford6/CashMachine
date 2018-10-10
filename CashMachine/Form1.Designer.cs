bc78ce0cf94baa9cc730fdcefbdbe9ccebaac4ce
AndrewLangford6 <andrew.langford.6@gmail.com>
2018-10-10 14:59:39 -0500

    Add project files.

---
+++ CashMachine/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace CashMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.p1UpDown = new System.Windows.Forms.NumericUpDown();
+            this.p2UpDown = new System.Windows.Forms.NumericUpDown();
+            this.p3UpDown = new System.Windows.Forms.NumericUpDown();
+            this.p4UpDown = new System.Windows.Forms.NumericUpDown();
+            this.p4 = new System.Windows.Forms.Label();
+            this.p3 = new System.Windows.Forms.Label();
+            this.p2 = new System.Windows.Forms.Label();
+            this.p1 = new System.Windows.Forms.Label();
+            this.mrg = new System.Windows.Forms.Label();
+            this.buy = new System.Windows.Forms.Button();
+            this.tInput = new System.Windows.Forms.TextBox();
+            this.amountTendered = new System.Windows.Forms.Label();
+            this.confirm = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.p1UpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2UpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3UpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4UpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // p1UpDown
+            // 
+            this.p1UpDown.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p1UpDown.Font = new System.Drawing.Font("Prestige Elite Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1UpDown.Location = new System.Drawing.Point(137, 58);
+            this.p1UpDown.Name = "p1UpDown";
+            this.p1UpDown.Size = new System.Drawing.Size(62, 21);
+            this.p1UpDown.TabIndex = 0;
+            // 
+            // p2UpDown
+            // 
+            this.p2UpDown.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p2UpDown.Font = new System.Drawing.Font("Prestige Elite Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2UpDown.Location = new System.Drawing.Point(137, 84);
+            this.p2UpDown.Name = "p2UpDown";
+            this.p2UpDown.Size = new System.Drawing.Size(62, 21);
+            this.p2UpDown.TabIndex = 1;
+            // 
+            // p3UpDown
+            // 
+            this.p3UpDown.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p3UpDown.Font = new System.Drawing.Font("Prestige Elite Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p3UpDown.Location = new System.Drawing.Point(137, 110);
+            this.p3UpDown.Name = "p3UpDown";
+            this.p3UpDown.Size = new System.Drawing.Size(62, 21);
+            this.p3UpDown.TabIndex = 2;
+            // 
+            // p4UpDown
+            // 
+            this.p4UpDown.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p4UpDown.Font = new System.Drawing.Font("Prestige Elite Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p4UpDown.Location = new System.Drawing.Point(137, 137);
+            this.p4UpDown.Name = "p4UpDown";
+            this.p4UpDown.Size = new System.Drawing.Size(62, 21);
+            this.p4UpDown.TabIndex = 3;
+            // 
+            // p4
+            // 
+            this.p4.AutoSize = true;
+            this.p4.Font = new System.Drawing.Font("Prestige Elite Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p4.Location = new System.Drawing.Point(22, 139);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(33, 12);
+            this.p4.TabIndex = 4;
+            this.p4.Text = "Rock";
+            // 
+            // p3
+            // 
+            this.p3.AutoSize = true;
+            this.p3.Font = new System.Drawing.Font("Prestige Elite Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p3.Location = new System.Drawing.Point(22, 112);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(47, 12);
+            this.p3.TabIndex = 5;
+            this.p3.Text = "Shells";
+            // 
+            // p2
+            // 
+            this.p2.AutoSize = true;
+            this.p2.Font = new System.Drawing.Font("Prestige Elite Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2.Location = new System.Drawing.Point(22, 86);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(33, 12);
+            this.p2.TabIndex = 6;
+            this.p2.Text = "Leaf";
+            // 
+            // p1
+            // 
+            this.p1.AutoSize = true;
+            this.p1.Font = new System.Drawing.Font("Prestige Elite Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1.Location = new System.Drawing.Point(22, 60);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(40, 12);
+            this.p1.TabIndex = 7;
+            this.p1.Text = "Stick";
+            // 
+            // mrg
+            // 
+            this.mrg.BackColor = System.Drawing.Color.Transparent;
+            this.mrg.Image = ((System.Drawing.Image)(resources.GetObject("mrg.Image")));
+            this.mrg.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.mrg.Location = new System.Drawing.Point(607, 93);
+            this.mrg.Name = "mrg";
+            this.mrg.Size = new System.Drawing.Size(581, 484);
+            this.mrg.TabIndex = 8;
+            this.mrg.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buy
+            // 
+            this.buy.Font = new System.Drawing.Font("Prestige Elite Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buy.Location = new System.Drawing.Point(24, 174);
+            this.buy.Name = "buy";
+            this.buy.Size = new System.Drawing.Size(75, 23);
+            this.buy.TabIndex = 9;
+            this.buy.Text = "BUY";
+            this.buy.UseVisualStyleBackColor = true;
+            this.buy.Click += new System.EventHandler(this.buyButton);
+            // 
+            // tInput
+            // 
+            this.tInput.Location = new System.Drawing.Point(24, 238);
+            this.tInput.Name = "tInput";
+            this.tInput.Size = new System.Drawing.Size(121, 20);
+            this.tInput.TabIndex = 10;
+            this.tInput.Visible = false;
+            // 
+            // amountTendered
+            // 
+            this.amountTendered.Font = new System.Drawing.Font("Prestige Elite Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amountTendered.Location = new System.Drawing.Point(24, 210);
+            this.amountTendered.Name = "amountTendered";
+            this.amountTendered.Size = new System.Drawing.Size(121, 25);
+            this.amountTendered.TabIndex = 11;
+            this.amountTendered.Text = "Amount Tendered";
+            this.amountTendered.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.amountTendered.Visible = false;
+            // 
+            // confirm
+            // 
+            this.confirm.Font = new System.Drawing.Font("Prestige Elite Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confirm.Location = new System.Drawing.Point(151, 235);
+            this.confirm.Name = "confirm";
+            this.confirm.Size = new System.Drawing.Size(75, 23);
+            this.confirm.TabIndex = 12;
+            this.confirm.Text = "CONFRIM";
+            this.confirm.UseVisualStyleBackColor = true;
+            this.confirm.Visible = false;
+            this.confirm.Click += new System.EventHandler(this.confirm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackgroundImage = global::CashMachine.Properties.Resources.murrrr;
+            this.ClientSize = new System.Drawing.Size(611, 477);
+            this.Controls.Add(this.confirm);
+            this.Controls.Add(this.amountTendered);
+            this.Controls.Add(this.tInput);
+            this.Controls.Add(this.buy);
+            this.Controls.Add(this.mrg);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p4UpDown);
+            this.Controls.Add(this.p3UpDown);
+            this.Controls.Add(this.p2UpDown);
+            this.Controls.Add(this.p1UpDown);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+        //    this.Text = "fds";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.p1UpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2UpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3UpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4UpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown p1UpDown;
+        private System.Windows.Forms.NumericUpDown p2UpDown;
+        private System.Windows.Forms.NumericUpDown p3UpDown;
+        private System.Windows.Forms.NumericUpDown p4UpDown;
+        private System.Windows.Forms.Label p4;
+        private System.Windows.Forms.Label p3;
+        private System.Windows.Forms.Label p2;
+        private System.Windows.Forms.Label p1;
+        private System.Windows.Forms.Label mrg;
+        private System.Windows.Forms.Button buy;
+        private System.Windows.Forms.TextBox tInput;
+        private System.Windows.Forms.Label amountTendered;
+        private System.Windows.Forms.Button confirm;
+    }
+}
+
---
+++ CashMachine/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CashMachine
+{
+    public partial class Form1 : Form
+    {
+        int mrgX = 607;
+        const int STICK = 5;
+        const int LEAF = 3;
+        const int SHELL = 30;
+        const int ROCK = 3;
+        const int FLAT_RATE = 2;
+        const double HST = 2.5;
+
+        const int x = 65;
+        const int y = 265;
+
+        Font Text = new Font("Prestige Elite Std", 10, FontStyle.Regular);
+        SolidBrush Paper = new SolidBrush(Color.White);
+        SolidBrush Fill = new SolidBrush(Color.Black);
+
+
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            /*
+        for (mrgX = 607; mrgX >= 307; mrgX = mrgX - 2)
+        {
+            mrg.Location = new Point(mrgX, 93);
+            this.Refresh();
+            Thread.Sleep(100);
+            */
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buyButton(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+
+            // string stickCount = ToString.p1UpDown;
+            double stickCount = Convert.ToInt16(p1UpDown.Value);
+            double totalP1 = STICK * stickCount;
+
+            double leafCount = Convert.ToInt16(p2UpDown.Value);
+            double totalP2 = LEAF * leafCount;
+
+            double shellCount = Convert.ToInt16(p3UpDown.Value);
+            double totalP3 = SHELL * shellCount;
+
+            double rockCount = Convert.ToInt16(p4UpDown.Value);
+            double totalP4 = ROCK * rockCount;
+
+            double totalP = totalP1 + totalP2 + totalP3 + totalP4;
+            double totalp_HST = totalP * HST;
+
+            try
+            {
+
+                g.FillRectangle(Paper, x, y, x * 4, 300);
+
+                g.DrawString("Mrgggle's Trade"
+               , Text, Fill, x + 65, y);
+
+                g.DrawString("Number of sticks:      " + stickCount
+                , Text, Fill, x, y + 25);
+
+                g.DrawString("Number of leaves:      " + leafCount
+                , Text, Fill, x, y + 40);
+
+                g.DrawString("Number of shells:      " + shellCount
+                , Text, Fill, x, y + 55);
+
+                g.DrawString("Number of rocks:       " + rockCount
+                , Text, Fill, x, y + 70);
+
+                g.DrawString("Subtotal:              " + totalP.ToString(",###.## gp")
+                , Text, Fill, x, y + 95);
+
+                g.DrawString("King Toggwagle's Tax:  " + (HST - 1).ToString("#.##%")
+                , Text, Fill, x, y + 110);
+
+                g.DrawString("Total:                 " + totalp_HST.ToString(",###.## gp")
+                , Text, Fill, x, y + 125);
+
+                if (stickCount == 0 && leafCount == 0 && shellCount == 0 && rockCount == 0)
+                {
+                    g.FillRectangle(Paper, x, y, x * 4, 300);
+
+                    g.DrawString("Hey, buddy.\nDon't waist my time."
+                    , Text, Fill, x + 65, y + 70);
+
+                    amountTendered.Visible = false;
+                    tInput.Visible = false;
+                    confirm.Visible = false;
+                }
+
+                if (stickCount != 0 || leafCount != 0 || shellCount != 0 || rockCount != 0)
+                {
+
+                    amountTendered.Visible = true;
+                    tInput.Visible = true;
+                    confirm.Visible = true;
+
+                }
+            }
+
+            catch
+            {
+                g.FillRectangle(Paper, x, y, x * 4, 300);
+
+                g.DrawString("Amount must be an number."
+                , Text, Fill, x, y + 70);
+            }
+        }
+
+        private void confirm_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+
+            double stickCount = Convert.ToInt16(p1UpDown.Value);
+            double totalP1 = STICK * stickCount;
+
+            double leafCount = Convert.ToInt16(p2UpDown.Value);
+            double totalP2 = LEAF * leafCount;
+
+            double shellCount = Convert.ToInt16(p3UpDown.Value);
+            double totalP3 = SHELL * shellCount;
+
+            double rockCount = Convert.ToInt16(p4UpDown.Value);
+            double totalP4 = ROCK * rockCount;
+
+            double totalP = totalP1 + totalP2 + totalP3 + totalP4;
+            double totalp_HST = totalP * HST;
+
+            double amount = Convert.ToInt16(tInput.Text);
+
+            double change = amount - totalp_HST;
+
+
+            g.FillRectangle(Paper, x, y, x * 4, 300);
+
+            g.DrawString("Mrgggle's Trade"
+           , Text, Fill, x + 65, y);
+
+            g.DrawString("Number of sticks:      " + stickCount
+            , Text, Fill, x, y + 25);
+
+            g.DrawString("Number of leaves:      " + leafCount
+            , Text, Fill, x, y + 40);
+
+            g.DrawString("Number of shells:      " + shellCount
+            , Text, Fill, x, y + 55);
+
+            g.DrawString("Number of rocks:       " + rockCount
+            , Text, Fill, x, y + 70);
+
+            g.DrawString("Subtotal:              " + totalP.ToString(",###.## gp")
+            , Text, Fill, x, y + 95);
+
+            g.DrawString("King Toggwagle's Tax:  " + (HST - 1).ToString("#.##%")
+            , Text, Fill, x, y + 110);
+
+            g.DrawString("Total:                 " + totalp_HST.ToString(",###.## gp")
+            , Text, Fill, x, y + 125);
+
+            g.DrawString("Tendered:              " + amount.ToString(",###.## gp")
+            , Text, Fill, x, y + 150);
+
+            g.DrawString("Change:                " + amount.ToString(",###.## gp")
+            , Text, Fill, x, y + 165);
+
+        }
+    }
+}
